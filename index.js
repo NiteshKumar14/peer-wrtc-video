d484a88bb18e801178a9506ca40ebb3d4cee70be
Niteshkumar14 <erniteshch@gmail.com>
2021-12-30 21:00:38 +0530

    nek

--- index.js
+++ index.js
@@ -60,12 +60,6 @@ const server= app.listen(port,()=>{
     console.log(`server is up and running on port ${port}`);
 });
 
-if(process.env.NODE_ENV='production'){
-  app.use(express.static('client/build') )
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
 
 const io = new Server(server,{
 
